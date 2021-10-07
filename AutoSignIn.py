17b4fba2ad13a4a6593e613630df24ff1b638a50
CRYSTER12 <61831660+CRYSTER12@users.noreply.github.com>
2021-10-07 12:50:47 +0800

    Update AutoSignIn.py

--- AutoSignIn.py
+++ AutoSignIn.py
@@ -11,10 +11,13 @@ from email.mime.text import MIMEText
 from email.header import Header
 import re  # 正则表达式
 import yagmail
+import random
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+
+time.sleep(random.randint(20,200))
 # 系统变量
 StuID = os.environ['STUID']
 PW = os.environ['PW']
